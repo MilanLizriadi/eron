d1de2929718179d52b86853d1a506c5f369c549e
MilanLizriadi <milanlizriadi@gmail.com>
2022-09-27 12:49:19 +0700

    Tambahin Parallax

---
+++ scripts/parallax.js
@@ -0,0 +1,29 @@
+let products1 = document.getElementsByClassName("product-1");
+new simpleParallax(products1,{
+    orientation: "right"
+});
+
+let products2 = document.getElementsByClassName("product-2");
+new simpleParallax(products2,{
+    orientation: "left"
+});
+
+let products3 = document.getElementsByClassName("product-3");
+new simpleParallax(products3,{
+    orientation: "right"
+});
+
+let products4 = document.getElementsByClassName("product-4");
+new simpleParallax(products4,{
+    orientation: "left"
+});
+
+let products5 = document.getElementsByClassName("product-5");
+new simpleParallax(products5,{
+    orientation: "right"
+});
+
+let products6 = document.getElementsByClassName("product-6");
+new simpleParallax(products6,{
+    orientation: "left"
+});
